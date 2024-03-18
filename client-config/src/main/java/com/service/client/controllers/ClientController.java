c2234f003b91889bbdff55389fa4b1ded055e995
hdlee <dev.fringe@gmail.com>
2024-03-18 15:04:39 +0900

    init

--- client-config/src/main/java/com/service/client/controllers/ClientController.java
+++ client-config/src/main/java/com/service/client/controllers/ClientController.java
@@ -1,60 +1,25 @@
 package com.service.client.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.log4j.Log4j2;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
-@RefreshScope
+@Log4j2
 public class ClientController {
-	Logger logger = LoggerFactory.getLogger(ClientController.class);
     
     @Value("${my.greeting.message}")
-    private String greetingMessage;
+    String greetingMessage;
     
     @Value("${app.message}")
-    private String appMessage;
+    String appMessage;
     
     @GetMapping("/client")
 	public String clientCall() {		
-		try {
-			logger.info("Client ");
-			System.out.println(appMessage);
-			return "Hi from Client";
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
+		log.info("Client ");
+		log.info(greetingMessage);
+		return "Hi from Client";
 	}
-    
-    
-    @GetMapping("/greeting")
-   	public String greeting() {		
-   		try {
-   			logger.info("Reading Greeting config from Config Server properties ");
-   			return "greeting config from config server is : "+greetingMessage;
-   		} catch (Exception e) {
-   			e.printStackTrace();
-   		}
-   		return null;
-   	}
-    
-    @GetMapping("/message")
-   	public String appMessage() {		
-   		try {
-   			logger.info("Reading Greeting config from Local properties ");
-   			return "greeting config from local properties server is : "+appMessage;
-   		} catch (Exception e) {
-   			e.printStackTrace();
-   		}
-   		return null;
-   	}
-    
-
 }
